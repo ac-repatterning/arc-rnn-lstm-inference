c0e98ae367861cce77dee2997080714a7bac1e76
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-11-04 22:34:39 +0000

    Update src/inference/scaling.py: In Progress

--- src/inference/scaling.py
+++ src/inference/scaling.py
@@ -12,8 +12,26 @@ class Scaling:
     def __init__(self):
         pass
 
-    def inverse_transform(self):
-        pass
+    @staticmethod
+    def inverse_transform(data: pd.DataFrame, scaling: dict):
+        """
+
+        :param data:
+        :param scaling:
+        :return:
+        """
+
+        # The values
+        structure: np.ndarray = data.copy()[scaling.get('feature_names_in_')].values
+
+        # The inverse transform of ...
+        matrix = np.multiply(structure, scaling.get('data_range_')) + scaling.get('data_min_')
+
+        # Hence
+        frame = pd.DataFrame()
+        frame.loc[:, scaling.get('feature_names_in_')] = matrix
+
+        return frame
 
     @staticmethod
     def transform(data: pd.DataFrame, scaling: dict) -> pd.DataFrame:
