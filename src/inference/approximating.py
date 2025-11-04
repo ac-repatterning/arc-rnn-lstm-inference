ef506798f19e58695dbd5e7101c513be62367cf9
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-11-04 22:54:35 +0000

    Update src/inference/approximating.py: In Progress

--- src/inference/approximating.py
+++ src/inference/approximating.py
@@ -9,16 +9,34 @@ import src.elements.specification as sc
 
 class Approximating:
 
-    def __init__(self):
+    def __init__(self, arguments: dict):
+        """
 
+        :param arguments:
+        """
+
+        self.__arguments = arguments
+
+        # Instances
         self.__configurations = config.Config()
 
-    def __get_model(self, path: str) -> tf.keras.models.Sequential:
+    @staticmethod
+    def __get_model(path: str) -> tf.keras.models.Sequential:
+        """
+
+        :param path:
+        :return:
+        """
 
         return tf.keras.models.load_model(
             filepath=os.path.join(path, 'model.keras'))
 
     def exc(self, specification: sc.Specification):
+        """
+
+        :param specification:
+        :return:
+        """
 
         path = os.path.join(self.__configurations.data_, str(specification.catchment_id), str(specification.ts_id))
         model = self.__get_model(path=path)
